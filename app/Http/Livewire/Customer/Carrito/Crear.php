016a89e38f30230a541abd895fd314e0e30b0d41
Xeexen <andres.o.mm@hotmail.com>
2024-02-20 20:37:46 -0500

    Carrito

--- app/Http/Livewire/Customer/Carrito/Crear.php
+++ app/Http/Livewire/Customer/Carrito/Crear.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Livewire\Customer\Carrito;
 
+use App\Models\Carrito;
 use Livewire\Component;
 
 class Crear extends Component
 {
+    public $carrito;
+
     public function mount()
     {
-        $this->carrito = Carrito::where('')
+        $this->carrito = Carrito::where('');
     }
     public function render()
     {
